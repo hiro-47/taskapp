85836c0e22247b0e47d52dfb5eeffdc07c025c09
hiro-47 <13160306@g.matsuyama-u.ac.jp>
2019-11-18 15:45:18 +0900

    一旦完成 こっから機能追加

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by KATAOKA  HIROTAKA on 2019/11/16.
+//  Copyright © 2019 hirotaka.kataoka. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
